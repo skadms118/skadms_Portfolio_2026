4ad9ca42d6b6548d554695dc05305e8d373ac298
skadms118 <skadms118@gmail.com>
2026-06-18 06:10:59 +0900

    피드백 반영 1차

--- src/experience/ExperiencePage.tsx
+++ src/experience/ExperiencePage.tsx
@@ -1,5 +1,4 @@
 import Experience from "./experience/Experience";
-import Qualification from "./qualification/Qualification";
 
 /**
  * /experience - Experience / Award / Qualification 세로 스크롤 섹션으로 구성된다.
@@ -8,7 +7,6 @@ function ExperiencePage() {
   return (
     <div>
       <Experience />
-      <Qualification />
     </div>
   );
 }
